336672ca306656e0ff2d5fde6d067d24c11f234a
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2025-02-25 18:20:00 +0530

    Update weatherObservation12.sql

--- weatherObservation12.sql
+++ weatherObservation12.sql
@@ -1,1 +1,4 @@
 SELECT DISTINCT city FROM station WHERE LEFT(city, 1) NOT IN ( 'a', 'e', 'i', 'o', 'u' ) AND RIGHT(city, 1) NOT IN ( 'a', 'e', 'i', 'o', 'u' );
+
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-12/problem?isFullScreen=true
